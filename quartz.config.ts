7bd48df340406f67b01f1e58fb012776a3e99e2d
juanwang01 <3078184064@qq.com>
2025-12-19 13:11:22 +0800

    添加首页配置 indexPage

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,6 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "Quartz 4",
     pageTitleSuffix: "",
+    indexPage: "index.md",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
